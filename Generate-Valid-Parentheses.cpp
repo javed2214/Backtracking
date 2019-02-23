1720c8a4533d12fb0e2ed021afa468066fc7a18d
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-23 11:33:00 +0530

    Add files via upload

---
+++ Generate-Valid-Parentheses.cpp
@@ -0,0 +1,39 @@
+// Program to Generate Valid Parentheses
+// Given n Pairs of Parentheses, Write a function to Generate All Combinations of Valid Parentheses
+// Ex: n=2 ()() and (())
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void getParenth(int openB, int closeB, int n, string str, vector <string> &v){
+
+	if(closeB==n){
+		v.emplace_back(str);
+		return;
+	}
+	if(closeB < openB){
+		str+=")";
+		getParenth(openB,closeB+1,n,str,v);
+		str.pop_back();
+	}
+	if(openB < n){
+		str+="(";
+		getParenth(openB+1,closeB,n,str,v);
+		str.pop_back();
+	}
+}
+
+int main(){
+
+	int n=3;
+	string str;
+	vector <string> v;
+	getParenth(0,0,n,str,v);
+
+	for(auto it:v){
+		for(auto ptr:it)
+			cout<<ptr;
+		cout<<"\n";
+	}
+	return 0;
+}
