0d8b2f220911d5a22791c786614070f20b3ddecc
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2020-12-28 00:44:06 +0530

    Update Root-To-Leaf-Sum-Equals-To-K.cpp

--- Root-To-Leaf-Sum-Equals-To-K.cpp
+++ Root-To-Leaf-Sum-Equals-To-K.cpp
@@ -1,4 +1,4 @@
-// // Program to Print All the Paths from Root to Leaf with a Specified Sum in Binary Tree
+// Program to Print All the Paths from Root to Leaf with a Specified Sum in Binary Tree
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -9,74 +9,55 @@ typedef struct node{
 	struct node *right;
 }node;
 
-node *createTree(node *root, int n){
-	if(root==NULL){
-		root=(node *)malloc(sizeof(node));
-		root->data=n;
-		root->left=NULL;
-		root->right=NULL;
-		return root;
-	}
-	else if(n<root->data)
-		root->left=createTree(root->left,n);
-	else if(n>root->data)
-		root->right=createTree(root->right,n);
 
-	return root;
+node *createNode(int x){
+
+	node *temp = (node *)malloc(sizeof(node));
+	temp->left = NULL;
+	temp->right = NULL;
+	temp->data = x;
+
+	return temp;		
 }
 
-// *********** EFFICIENT APPROACH **************
-/*
-cnt is set to ZERO
-void getPath(TreeNode *root, int cnt, int sum, vector<int> &v, vector<vector<int>> &res){
-    
-    if(root==NULL) return;
-    cnt+=root->val;
-    v.push_back(root->val);
-    if(!root->left and !root->right){
-        if(cnt==sum) res.push_back(v);
-    } 
-    getPath(root->left,cnt,sum,v,res);
-    getPath(root->right,cnt,sum,v,res);
-    cnt-=root->val;
-    v.pop_back();
-}
-
-*/
 
 void printPaths(vector<int>v){
-	for(auto it:v) cout<<it<<" ";
-		cout<<"\n";
+	for(auto it:v) cout << it << " ";
+		cout << "\n";
 }
 
 void getPaths(node *root, vector<int> &v, int target, int sum){
 
-	if(root==NULL) return;
+	if(root == NULL) return;
 	
 	v.push_back(root->data);
-	sum+=root->data;
-	if(sum==target and root->left==NULL and root->right==NULL)
+
+	sum += root->data;
+	
+	if(sum == target and root->left == NULL and root->right == NULL)
 		printPaths(v);
 
 	getPaths(root->left,v,target,sum);
 	getPaths(root->right,v,target,sum);
+	
 	v.pop_back();
 }
 
 int main(){
 
-	int a[]={20,10,5,15,40,25,45};
-	int n=sizeof(a)/sizeof(int);
-	int target=45;
-	
-	node *root=NULL;
-	
-	for(int i=0;i<n;i++)
-		root=createTree(root,a[i]);
+	node *root = createNode(2);
+	root->left = createNode(7);
+	root->right = createNode(4);
+	root->left->left = createNode(6);
+	root->left->right = createNode(8);
+	root->right->left = createNode(9);
+	root->right->right = createNode(1);
+
+	int target = 17;
 	
 	vector<int> v;
 	
-	getPaths(root,v,target,0);
+	getPaths(root, v, target, 0);
 
 	return 0;
 }
